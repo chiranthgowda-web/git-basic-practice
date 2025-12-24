b3112e112eb1401c6fa974f8fe15719138bd78cf
Chiranth Gowda <chiranthgowda.kasige@gmail.com>
2025-12-24 13:34:41 +0530

    I have added a Coomand on Script.js

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
 // Indian Watch
-let indDate = document.querySelector('#indian-date');
-let indTime = document.querySelector('#indian-time');
-let indianWatch = ()=>{
-    let options = { timeZone : 'Asia/Kolkata'};
-    let currentDate = new Date().toLocaleDateString('en-US',options);
-    let currentTime = new Date().toLocaleTimeString('en-US',options);
-    indDate.innerText = currentDate;
-    indTime.innerText = currentTime;
+// Updating to Github Account
+let indDate = document.querySelector("#indian-date");
+let indTime = document.querySelector("#indian-time");
+let indianWatch = () => {
+  let options = { timeZone: "Asia/Kolkata" };
+  let currentDate = new Date().toLocaleDateString("en-US", options);
+  let currentTime = new Date().toLocaleTimeString("en-US", options);
+  indDate.innerText = currentDate;
+  indTime.innerText = currentTime;
 };
-setInterval(indianWatch,1000);
+setInterval(indianWatch, 1000);
 
 // USA Watch
-let usaDate = document.querySelector('#usa-date');
-let usaTime = document.querySelector('#usa-time');
-let usaWatch = ()=>{
-    options = { timeZone : 'America/New_York'};
-    currentDate = new Date().toLocaleDateString('en-US',options);
-    currentTime = new Date().toLocaleTimeString('en-Us',options);
-    usaDate.innerText = currentDate;
-    usaTime.innerText= currentTime;
+let usaDate = document.querySelector("#usa-date");
+let usaTime = document.querySelector("#usa-time");
+let usaWatch = () => {
+  options = { timeZone: "America/New_York" };
+  currentDate = new Date().toLocaleDateString("en-US", options);
+  currentTime = new Date().toLocaleTimeString("en-Us", options);
+  usaDate.innerText = currentDate;
+  usaTime.innerText = currentTime;
 };
-setInterval(usaWatch,1000);
+setInterval(usaWatch, 1000);
 
 //China Watch
-let chinaDate = document.querySelector('#china-date');
-let chinaTime = document.querySelector('#china-time');
-let chinaWatch = ()=>{
-    options = { timeZone : 'Asia/Shanghai'};
-    currentDate = new Date().toLocaleDateString('en-US',options);
-    currentTime = new Date().toLocaleTimeString('en-US',options);
-    chinaDate.innerText = currentDate;
-    chinaTime.innerText = currentTime;
+let chinaDate = document.querySelector("#china-date");
+let chinaTime = document.querySelector("#china-time");
+let chinaWatch = () => {
+  options = { timeZone: "Asia/Shanghai" };
+  currentDate = new Date().toLocaleDateString("en-US", options);
+  currentTime = new Date().toLocaleTimeString("en-US", options);
+  chinaDate.innerText = currentDate;
+  chinaTime.innerText = currentTime;
 };
-setInterval(chinaWatch,1000);
+setInterval(chinaWatch, 1000);
